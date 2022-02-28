72731d720357f63abafb54908b0eb5bf5e54423c
dj-judd <dj.judd@gmail.com>
2022-02-28 01:42:05 -0600

    finished Step 2 :')

---
+++ src/app/StreamSorting.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import entity.Camera;
+
+public class StreamSorting {
+	
+	public static void main(String[] args) {
+	
+		String[] cameraNames = {"Canon", "Nikon", "Sony", "Panasonic", "Red", "BlackMagic", "Sigma"};
+		List<Camera> cameraList = new ArrayList<Camera>();
+
+		for (int i = 0; i < cameraNames.length; i++) {
+			cameraList.add(new Camera(cameraNames[i]));
+		}
+		
+		String streamSorted = cameraList.stream().map(c -> c.getName()).sorted().collect(Collectors.joining(", "));
+		
+		System.out.println("Cameras sorted by name:\n" + streamSorted);
+		
+		
+		
+	}
+}
